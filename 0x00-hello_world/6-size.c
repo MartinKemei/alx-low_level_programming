632c6f594d0fc71415304a6301f0849d53886728
MartinKemei <martinkemei254@gmail.com>
2022-12-08 09:26:47 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	return (0);
+}
