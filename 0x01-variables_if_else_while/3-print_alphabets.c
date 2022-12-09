9d40d6e59db74248208752952e86a9842d12bc71
MartinKemei <martinkemei254@gmail.com>
2022-12-09 08:03:03 -0800

    project 0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char b;
+	char c;
+
+	while
+		(b <= 'a') {
+			putchar(b);
+			b++;
+		}
+	while
+		(c <= 'A') {
+			putchar(c);
+			c++;
+		}
+	return (0);
+}
+
