d5e55bcf4db4caeeb336f003cccd89fd74ba0195
MartinKemei <martinkemei254@gmail.com>
2022-12-16 08:51:58 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * mul - multiplication function
+ * @a: first int
+ * @b: second int
+ * Return: multiplication result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
