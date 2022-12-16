9ccff0a57ce21288eea6f959eb06ab98100013c0
MartinKemei <martinkemei254@gmail.com>
2022-12-16 10:47:04 -0800

    errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * main - print most numbers
+ * main - Check the code
+ *
  * Return: void
  */
 
-void print_most_numbers(void);
+
+void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0' ; c <= '9' ; c++)
 	{
 		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
