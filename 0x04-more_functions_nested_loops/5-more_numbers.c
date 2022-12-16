9d7ad8bae159b1c8f02e1b9dc1be39113e433f8a
MartinKemei <martinkemei254@gmail.com>
2022-12-16 11:19:01 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,20 @@
  * Return: void
  */
 
-
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	char c;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (c = 0; c <= 14; c++)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar (j % 10 + '0');
+			if (c > 9)
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
+
