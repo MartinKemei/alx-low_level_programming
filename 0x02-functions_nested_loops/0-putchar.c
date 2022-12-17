514d7cea05ccc1cd74058b67ef0ab87ea2ee234f
MartinKemei <martinkemei254@gmail.com>
2022-12-17 04:48:37 -0800

    added prototypes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,12 @@
 /**
  * main - _putchar followed by a new line
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
