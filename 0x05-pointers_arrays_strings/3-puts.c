512dd61f296bb7df601aa2ba68c93cd910dbd989
MartinKemei <martinkemei254@gmail.com>
2022-12-19 12:29:18 -0800

    fixing errors

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,17 +7,12 @@
  */
 void _puts(char *str)
 {
-	int count = 0;
+	int a = 0;
 
-	while (count >= 0)
+	while (str[a] != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(str[a]);
+		a++;
 	}
+	_putchar('\n');
 }
-
