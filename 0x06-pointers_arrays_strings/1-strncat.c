bac7eb2c425ed48b38fa3af17c039f72ad8eb85a
MartinKemei <martinkemei254@gmail.com>
2022-12-21 10:10:58 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,23 @@
  *
  * Return: dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	char *dest_end = dest + strlen(dest);
 	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	dest_end[i] = src[i];
-	dest_end[i] = '\0';
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
