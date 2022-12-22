4508d697a6a3eea0dee7620d0a71b6ead9701a4a
MartinKemei <martinkemei254@gmail.com>
2022-12-22 01:08:33 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,12 +11,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
+	int i;
+	int tmp;
 
 	for (i = 0; i < n / 2; i++)
 	{
-	tmp = a[i];
-	a[i] = a[n];
-	a[n] = tmp;
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
 	}
 }
