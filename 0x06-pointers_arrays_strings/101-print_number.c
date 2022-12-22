44b7bc2ebd2009c50fdea50d9b8e6696ab93c389
MartinKemei <martinkemei254@gmail.com>
2022-12-22 12:16:19 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,25 +2,27 @@
 
 /**
  * rot13 - encodes a string in rot13
- * @str: string to be encoded
+ * @s: string to be encoded
  *
  * Return: the resulting string
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int i;
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	if (str[i] >= 'A' && str[i] <= 'Z')
-	{
-		str[i] = (str[i] - 'A' + 13) % 26 + 'A';
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
 	}
-	else if (str[i] >= 'a' && str[i] <= 'z')
-	{
-	str[i] = (str[i] - 'a' + 13) % 26 + 'a';
-	}
-	}
-	return (str);
+	return (s);
 }
