077d57d9692e017cc52d892116f06e6a7da24a3e
MartinKemei <martinkemei254@gmail.com>
2023-05-18 04:37:58 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
 
 	return (1);
 }
