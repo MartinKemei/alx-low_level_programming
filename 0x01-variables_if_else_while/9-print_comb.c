4c4c8d1bc7e49f25ce91250c3665816cf670038b
MartinKemei <martinkemei254@gmail.com>
2023-05-19 15:17:24 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - prints combinations of single-digit numbers (0-9)
+ *in ascending order
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+
+		if (num != 9)
+		{
+			putchar(',');
+		}
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
