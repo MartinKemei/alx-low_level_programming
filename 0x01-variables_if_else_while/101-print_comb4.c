f85970e8101a433f394b70ab10976a9be8a92e74
MartinKemei <martinkemei254@gmail.com>
2023-05-20 00:07:50 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - prints out all possible combinations of
+ *three digits
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (1)
+	{
+		putchar(num / 100 + '0');
+		putchar(num / 10 +'0');
+		putchar(num % 10 + '0');
+
+		if (num !=789)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			break;
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
