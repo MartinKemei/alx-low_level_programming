732a98cac6f54b24743d3d37099650f016fbc2d4
MartinKemei <martinkemei254@gmail.com>
2023-05-23 13:24:00 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet in lowercase
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
