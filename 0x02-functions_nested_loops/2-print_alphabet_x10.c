340d60942708abf3ad355bd2dcf139f82d926a15
MartinKemei <martinkemei254@gmail.com>
2023-05-23 14:33:10 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,18 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		char alpha = 'a';
+
 		while (alpha <= 'z')
 		{
 			putchar(alpha);
 			alpha++;
 		}
+
 		i++;
 	}
+
 	putchar('\n');
 }
