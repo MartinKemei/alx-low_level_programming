69939432e3df404a24392fe20bc488b5e5702e21
MartinKemei <martinkemei254@gmail.com>
2023-05-24 06:45:53 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - a program that adds two integers and returns result
+ *
+ *@a: int one
+ *@b: int two
+ *
+ *Return: result
+ */
+
+int add(int a, int b)
+{
+	result = a + b;
+	return (result);
+}
