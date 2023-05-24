435064ac65883b6dcb2d6ce7977259113ac4cef6
MartinKemei <martinkemei254@gmail.com>
2023-05-24 08:24:17 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *print_to_98 - prints numbers from n to 98
+ *
+ *Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+
+	else if (n >=98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+
+	printf("%d\n", n);
+}
