0ab69a413e97b3598f98816f3386abb877ff4805
MartinKemei <martinkemei254@gmail.com>
2023-05-24 04:32:23 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_abs - prints absolute value of an int
+ *
+ *Return: Always 0
+ */
+
+int _abs(int)
+{
+	int i;
+
+	if (i > 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
