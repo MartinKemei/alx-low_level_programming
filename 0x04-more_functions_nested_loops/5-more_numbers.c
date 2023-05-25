b6907a41b04e5cdcfd9d71566966bbb4bce418dc
MartinKemei <martinkemei254@gmail.com>
2023-05-25 16:45:25 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,16 @@
 
 void more_numbers(void)
 {
-	int number = 0;
+	int number, repeat;
 
-	for (number = 0; number <= 14; number++)
+	for (repeat = 0; repeat <= 10; repeat++)
 	{
-		for (number = 0; number < 10; number++)
+		for (number = 0; number <= 14 10; number++)
 		{
-			_putchar(number + '0');
-			number++;
+			if (number > 9)
+				_putchar(number / 10 + '0');
+			_putchar(number % 10 + '0');
 		}
-		_putchar(number + '0');
-		number++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
