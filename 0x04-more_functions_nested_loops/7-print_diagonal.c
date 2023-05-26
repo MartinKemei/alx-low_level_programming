be560373686aa9605b3928c413fe2f14fd443523
MartinKemei <martinkemei254@gmail.com>
2023-05-26 03:34:38 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_diagonal - draws a diagonal line
+ *
+ *@n:
+ *Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 1;
+
+		while (i <= n)
+		{
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 int mul(int a, int b);
 void print_most_numbers(void);
+void print_diagonal(int n);
 void print_numbers(void);
 void print_line(int n);
 void more_numbers(void);
