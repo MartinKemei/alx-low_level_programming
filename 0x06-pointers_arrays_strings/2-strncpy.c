bac7bfed81c8579f5ba4f24fc926c0239d85583b
MartinKemei <martinkemei254@gmail.com>
2023-06-06 04:23:57 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
+		dest[i] = src[i];
 	}
 
-	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	for (; i < n; i++)
 	{
-		dest[i] = src[j];
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 
 	return (dest);
 }
