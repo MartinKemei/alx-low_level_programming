7e2862cb46703a03b596ec2066f82cd3c85da463
MartinKemei <martinkemei254@gmail.com>
2023-06-06 07:37:04 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,30 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string
- * @str: input string
+ * @str: - inputs string
  * Return: capitalized string
  */
+
 char *cap_string(char *str)
 {
-	int i, j;
-	int separators[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
-			     '(', ')', '{', '}' };
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] -= 32;
-	for (i = 0; str[i] != '\0'; i++)
+	int count = 0, i;
+	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(str + count) >= 97 && *(str + count) <= 122)
+		*(str + count) = *(str + count) - 32;
+
+	while (*(str + count) != '\0')
 	{
-		for (j = 0; j < sizeof(separators) / sizeof(separators[0]);
-		     j++)
+		for (i = 0; i < 13; i++)
 		{
-			if (str[i] == separators[j])
+			if (*(str + count) == sep[i])
 			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-					str[i + 1] -= 32;
+				if ((*(str + (count + 1)) >= 97) && (*(str + (count + 1)) <= 122))
+					*(str + (count + 1)) = *(str + (count + 1)) - 32;
 				break;
 			}
 		}
+		count++;
 	}
 	return (str);
 }
